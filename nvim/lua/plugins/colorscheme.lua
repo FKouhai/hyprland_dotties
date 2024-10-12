d929817539647aff9d4ce1ee22234e68b9aa6c4e
FKouhai <frandres00@gmail.com>
2024-10-12 13:36:06 +0200

    new configs
    This commit includes the following updates:
    - Updated colorscheme to use tokyodark
    - Included harpoon configured to use telescope as the UI

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,14 @@
 return {
-  "folke/tokyonight.nvim",
-  config = function()
-    local tokyonight = require("tokyonight")
+  "tiagovla/tokyodark.nvim",
+   config = function()
+    local tokyonight = require("tokyodark")
     tokyonight.setup({
-      style = "storm",
-    })
-    vim.cmd[[colorscheme tokyonight-night]]
+      transparent_background = false,
+      gamma = 1.00,
+      comments = { italic = true }, -- style for comments
+      keywords = { italic = true }, -- style for keywords
+      identifiers = { italic = true }, -- style for identifiers
+   })
+    vim.cmd[[colorscheme tokyodark]]
   end,
 }
